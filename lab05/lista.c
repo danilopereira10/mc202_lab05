31d291de3d760be4795711ff710608160b9715f6
danilopereira10 <54330234+danilopereira10@users.noreply.github.com>
2019-10-05 15:18:40 -0300

    Changes on streucts

--- lab05/lista.c
+++ lab05/lista.c
@@ -2,25 +2,28 @@
 
 #include "lista.h"
 
-p_no create_list() {
+p_no create_list(int newSpace) {
 	p_no head = malloc(sizeof(p_no));
 	p_no newList = malloc(sizeof(p_no));
 	
 	head->next = newList;
 	head->prev = newList;
-	head->used = -1;
+	head->freeSpace = 0;
 	
 	newList->next = head;
 	newList->prev = head;
-	newList->used = 0;
+	newList->freeSpace = newSpace;
+	newList->initialPosition = 0;
 	return newList;
 }
 
-p_no add_no(p_no list) {
+/*p_no add_no(p_no list) {
+	
 	p_no newNo = malloc(sizeof(p_no));
 	newNo->next = list->next;
 	newNo->prev = list;
 	newNo->used = 0;
 	list->next = newNo;
 	return newNo;
-}
+	
+}*/
